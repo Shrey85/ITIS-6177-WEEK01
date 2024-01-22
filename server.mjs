b23c2e3fd3b6c21eb08fc1a89ae7718c6810a779
Shrey85 <157231201+Shrey85@users.noreply.github.com>
2024-01-21 21:17:30 -0500

    server.mjs

--- server.mjs
+++ server.mjs
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Welcome to my server!');
+  res.send('Hello from Shresta varma balaraju');
 });
 
 app.listen(port, () => {
